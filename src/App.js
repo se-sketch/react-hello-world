15822a8c95ad9f329bbec528d8e6f9f68349a630
se-sketch <setestgm20acc@gmail.com>
2022-11-11 08:56:11 +0200

    methods as props

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
+
+      <ParentComponent />
+
+      <br/>
 
       <FunctionClick />
       <br/>
