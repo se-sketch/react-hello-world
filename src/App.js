57ced75a5afd658b960a0d5b920412b9c52e75f3
se-sketch <setestgm20acc@gmail.com>
2022-11-12 10:21:02 +0200

    form

--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
 import './appStyles.css';
 import styles from './appStyles.module.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
 
-      {console.log(styles)}
+      <Form />
+
+    {/* 
 
       <h1 className='error'>Error</h1>
       <h1 className={styles.success}>Success</h1>
@@ -47,6 +50,8 @@ function App() {
       <ClassClick />
 
       <EventBind />
+
+      */}
 
       {/* 
       <Counter />
